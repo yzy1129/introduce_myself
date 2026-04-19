6c0142ce3f549bcd01f580ac46a918312aeb3685
yzyandhh <3188721533@qq.com>
2026-04-19 17:25:07 +0800

    删除冗余文件夹

--- codex_patch_20260418_143828/src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import { HeroSection } from "@/sections/HeroSection";
-
-export function HomePage() {
-  return (
-    <main id="main-content" tabIndex={-1} className="app-main app-main-home">
-      <HeroSection />
-    </main>
-  );
-}
